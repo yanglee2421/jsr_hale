9e103577ee6ea0a04e12b2211a08ef0f014402e2
yang <yanglee2421@gmail.com>
2024-10-29 19:39:11 +0800

    feat: add ws

--- main.ts
+++ main.ts
@@ -1,6 +1,25 @@
-const wk = new Worker(new URL("./worker.ts", import.meta.url), {
-  type: "module",
-});
+let data = "hale";
 
-console.log("main", new Date().toLocaleString());
-wk.onmessage = wk.terminate.bind(wk);
+const clients = new Set<WebSocket>();
+
+Deno.serve({ port: 8080 }, (req) => {
+  if (req.headers.get("upgrade") !== "websocket") {
+    return new Response(null, { status: 501 });
+  }
+  const { socket, response } = Deno.upgradeWebSocket(req);
+  socket.addEventListener("open", () => {
+    socket.send(data);
+    clients.add(socket);
+    console.log("client count:", clients.size);
+  });
+  socket.addEventListener("close", () => {
+    clients.delete(socket);
+    console.log("client count:", clients.size);
+  });
+  socket.addEventListener("message", (event) => {
+    data = event.data;
+    clients.forEach((socket) => socket.send(data));
+  });
+  socket.addEventListener("error", () => {});
+  return response;
+});
