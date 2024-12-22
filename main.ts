688f400e78bd999a0b7327aab62e04a578966b9f
yanglee2421 <yanglee2421@gmail.com>
2024-12-23 00:22:43 +0800

    feat add fn decode sse

--- main.ts
+++ main.ts
@@ -25,3 +25,42 @@ Deno.serve({ port: 8080 }, (req) => {
 
   return response;
 });
+
+const sseBuf = `id: 12345
+event: update
+retry: 5000
+data: {
+data:   "message": "Hello, this is an update!",
+data:   "timestamp": "2024-12-22T14:25:00Z"
+data: }
+
+:comment
+id: 12345
+event: update
+retry: 5000
+data: {
+data:   "message": "Hello, this is an update!",
+data:   "timestamp": "2024-12-22T14:25:00Z"
+data: }
+`;
+
+const idReg = /^id:(?<id>.+)$/m;
+const eventReg = /^event:(?<event>.+)$/m;
+const retryReg = /^retry:(?<retry>.+)$/m;
+const dataReg = /^data:(?<data>.+)$/mg;
+
+const decodeSSE = (buf: string) => {
+  return buf.split("\n\n").map((i) => {
+    const id = idReg.exec(i)?.groups?.id.trim();
+    const event = eventReg.exec(i)?.groups?.event.trim();
+    const retry = retryReg.exec(i)?.groups?.retry.trim();
+    const data = Array.from(i.matchAll(dataReg), (i) => i.groups?.data || "")
+      .join("");
+
+    return { id, event, retry, data };
+  });
+};
+
+console.log(
+  decodeSSE(sseBuf).map((i) => ({ ...i, data: JSON.parse(i.data) })),
+);
