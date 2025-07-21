4bcfd2e0cdad3c8edf4ca7f59cde0877ff623089
yanglee2421 <yanglee2421@gmail.com>
2025-07-21 12:30:09 +0800

    wip[webview]: lab for deno webview

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { Webview } from "@webview/webview";
+
+const html = `
+  <html>
+  <body>
+    <h1>Hello from deno v${Deno.version.deno}</h1>
+  </body>
+  </html>
+`;
+
+const webview = new Webview();
+
+webview.navigate(`data:text/html,${encodeURIComponent(html)}`);
+webview.run();
