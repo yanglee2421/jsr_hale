446c8fd45f85f6212f457cb72cd91a2aed5af331
yanglee2421 <yanglee2421@gmail.com>
2025-10-29 16:30:34 +0800

    chore: Add build

---
+++ src/build.ts
@@ -0,0 +1,9 @@
+Bun.build({
+  entrypoints: [Bun.fileURLToPath(new URL("./index.ts", import.meta.url))],
+  outdir: Bun.fileURLToPath(new URL("../dist", import.meta.url)),
+  naming: {
+    entry: "[name]-[hash].[ext]",
+    chunk: "[name]-[hash].[ext]",
+    asset: "[name]-[hash].[ext]",
+  },
+});
