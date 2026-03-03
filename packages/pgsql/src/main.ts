91ea2114d4dc280e24163e783be350f55b86f234
yanglee2421 <yanglee2421@gmail.com>
2026-03-03 17:28:45 +0800

    add fake rows

--- packages/pgsql/src/main.ts
+++ packages/pgsql/src/main.ts
@@ -27,12 +27,67 @@ const main = async () => {
     { count: 200 },
   );
 
-  const rows = await db
-    .insert(schema.chargingCabinetTable)
-    .values(values)
-    .returning();
+  const sockets = faker.helpers.multiple(
+    () => {
+      return {
+        socket_type: faker.helpers.arrayElement(["fast", "slow", "compatible"]),
+        status: faker.helpers.arrayElement([
+          "available",
+          "in_use",
+          "fault",
+          "maintenance",
+        ]),
+        create_time: faker.date.past().toISOString(),
+        update_time: faker.date.recent().toISOString(),
+        socket_code: faker.string.alphanumeric(10),
+        cabinet_id: faker.number.int({ min: 1, max: 600 }),
+        rated_voltage: faker.string.numeric(3),
+        rated_current: faker.string.numeric(3),
+        rated_power: faker.string.numeric(3),
+        last_used_time: faker.date.recent().toISOString(),
+        fault_note: faker.lorem.sentence(),
+      };
+    },
+    { count: 200 },
+  );
 
-  console.log(rows);
+  const orders = faker.helpers.multiple(
+    () => {
+      return {
+        payment_status: faker.helpers.arrayElement([
+          "pending",
+          "paid",
+          "failed",
+          "refunded",
+        ]),
+        order_status: faker.helpers.arrayElement([
+          "pending",
+          "in_progress",
+          "completed",
+          "cancelled",
+        ]),
+        create_time: faker.date.past().toISOString(),
+        update_time: faker.date.recent().toISOString(),
+        cabinet_id: faker.number.int({ min: 200, max: 300 }),
+        user_id: faker.helpers.arrayElement([1, 3, 4, 5]),
+        socket_id: faker.number.int({ min: 210, max: 300 }),
+        start_time: faker.date.past().toISOString(),
+        end_time: faker.date.recent().toISOString(),
+        total_kwh: "0",
+        total_amount: "0",
+        paid_amount: "0",
+      };
+    },
+    { count: 200 },
+  );
+
+  await db.transaction(async (tx) => {
+    await tx.insert(schema.chargingCabinetTable).values(values);
+    await tx.insert(schema.chargingSocketTable).values(sockets);
+    await tx.insert(schema.chargingOrderTable).values(orders);
+  });
+
+  console.log("Finished inserting data");
 };
 
 main();
