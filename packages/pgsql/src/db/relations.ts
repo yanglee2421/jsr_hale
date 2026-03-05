5c2be8086a6666b9c1fc78724ff8bad693c93668
yanglee2421 <yanglee2421@noreply.gitcode.com>
2026-03-05 15:13:57 +0800

    chore: Add seed

--- packages/pgsql/src/db/relations.ts
+++ packages/pgsql/src/db/relations.ts
@@ -3,27 +3,92 @@ import * as schema from "./schema";
 
 export const relations = defineRelations(schema, (r) => {
   return {
+    // 用户、会话、订单、反馈的关系
     userTable: {
-      orders: r.many.chargingOrderTable({
+      sessions: r.many.sessionTable({
         from: r.userTable.id,
-        to: r.chargingOrderTable.user_id,
+        to: r.sessionTable.user_id,
+      }),
+      orders: r.many.orderTable({
+        from: r.userTable.id,
+        to: r.orderTable.user_id,
+      }),
+      paymentRecords: r.many.paymentRecordTable({
+        from: r.userTable.id,
+        to: r.paymentRecordTable.user_id,
       }),
       feedbacks: r.many.userFeedbackTable({
         from: r.userTable.id,
         to: r.userFeedbackTable.user_id,
       }),
     },
-    chargingCabinetTable: {
-      sockets: r.many.chargingSocketTable({
-        from: r.chargingCabinetTable.id,
-        to: r.chargingSocketTable.cabinet_id,
+    sessionTable: {
+      user: r.one.userTable({
+        from: r.sessionTable.user_id,
+        to: r.userTable.id,
       }),
     },
-    chargingSocketTable: {
-      cabinet: r.one.chargingCabinetTable({
-        from: r.chargingSocketTable.cabinet_id,
-        to: r.chargingCabinetTable.id,
+
+    // 柜子和插座的关系
+    cabinetTable: {
+      sockets: r.many.socketTable({
+        from: r.cabinetTable.id,
+        to: r.socketTable.cabinet_id,
       }),
     },
+    socketTable: {
+      cabinet: r.one.cabinetTable({
+        from: r.socketTable.cabinet_id,
+        to: r.cabinetTable.id,
+      }),
+      runningRecords: r.many.socketRunningRecordTable({
+        from: r.socketTable.id,
+        to: r.socketRunningRecordTable.socket_id,
+      }),
+    },
+    socketRunningRecordTable: {
+      socket: r.one.socketTable({
+        from: r.socketRunningRecordTable.socket_id,
+        to: r.socketTable.id,
+      }),
+      order: r.one.orderTable({
+        from: r.socketRunningRecordTable.order_id,
+        to: r.orderTable.id,
+      }),
+    },
+
+    // 订单和支付记录的关系
+    orderTable: {
+      user: r.one.userTable({
+        from: r.orderTable.user_id,
+        to: r.userTable.id,
+      }),
+      cabinet: r.one.cabinetTable({
+        from: r.orderTable.cabinet_id,
+        to: r.cabinetTable.id,
+      }),
+      socket: r.one.socketTable({
+        from: r.orderTable.socket_id,
+        to: r.socketTable.id,
+      }),
+      paymentRecords: r.many.paymentRecordTable({
+        from: r.orderTable.id,
+        to: r.paymentRecordTable.order_id,
+      }),
+    },
+    paymentRecordTable: {
+      order: r.one.orderTable({
+        from: r.paymentRecordTable.order_id,
+        to: r.orderTable.id,
+      }),
+      user: r.one.userTable({
+        from: r.paymentRecordTable.user_id,
+        to: r.userTable.id,
+      }),
+    },
+
+    // 设备维护和用户反馈的关系
+    deviceMaintenanceTable: {},
+    userFeedbackTable: {},
   };
 });
--- packages/pgsql/src/main.ts
+++ packages/pgsql/src/main.ts
@@ -1,15 +1,14 @@
-import { faker } from "@faker-js/faker";
+import { fakerZH_CN as faker } from "@faker-js/faker";
 import { createDB } from "./db";
 import * as schema from "./db/schema";
-
-const statusOptions = ["available", "maintenance", "fault"] as const;
-type Status = (typeof statusOptions)[number];
+import { seed } from "drizzle-seed";
 
 const main = async () => {
   const dbPath = "postgresql://postgres:test123456@localhost:5432/postgres";
   const db = createDB(dbPath);
+  void faker;
 
-  await db.transaction(async (tx) => {});
+  await seed(db, schema, { count: 100 });
 
   console.log("Finished Database Update");
 };
